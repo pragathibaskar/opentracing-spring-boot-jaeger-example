28c87b319707a01b138d323a692b9d6bc5036d12
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-05 15:36:16 +0530

    Update HelloApplication.java

--- hello-service/src/main/java/com/hello/HelloApplication.java
+++ hello-service/src/main/java/com/hello/HelloApplication.java
@@ -1,12 +1,24 @@
 package com.hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan(basePackages="com.hello.*")
 public class HelloApplication implements WebMvcConfigurer {
 
+	 @Bean
+	    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+	        return restTemplateBuilder.build();
+	    }
+	    
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
