d12f711ff4a91c580a77ac94c591161328915f6a
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-05 15:37:02 +0530

    added chaining

--- hello-service/src/main/java/com/hello/rest/HelloController.java
+++ hello-service/src/main/java/com/hello/rest/HelloController.java
@@ -4,14 +4,22 @@ import com.hello.service.HelloService;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@RestController("/hello")
+@RestController
+@RequestMapping("hello")
 public class HelloController {
 
     @Autowired
     private Tracer tracer;
+    
+    @Autowired 
+    private RestTemplate template;
+    
 
     private final HelloService helloService;
 
@@ -20,11 +28,18 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
+    @GetMapping("/get")
     public String getHello() {
         Span span = tracer.buildSpan("HelloController getHello").start();
-        String result = helloService.printHello();
+        String result = "Hello from hello-service";
         span.finish();
         return result;
+    }
+    
+    @GetMapping("/chaining")
+    public String getChain() {
+    	ResponseEntity<String> response = template.getForEntity("http://localhost:8200/hello/get", String.class);
+        return "Chaining + " + response.getBody();
+    	
     }
 }
